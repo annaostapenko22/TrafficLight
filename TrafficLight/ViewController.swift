32623a78d8b12918a23e9f6c1b1378c46795618f
Anna Ostapenko <kvintabudinvest@gmail.com>
2021-05-19 00:34:58 +0200

    finished logic for traffic light cooler switching

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var button: UIButton!
+    
+    var currentClick = ""
+    
+    @IBAction func onClick(_ sender: UIButton) {
+        button.setTitle("NEXT", for: .normal)
+        redLight.layer.opacity = 1
+        switch currentClick {
+        case "red":
+            redLight.layer.opacity = 1
+            greenLight.layer.opacity = 0.3
+            currentClick = "yellow"
+        case "yellow":
+            yellowLight.layer.opacity = 1
+            redLight.layer.opacity = 0.3
+            currentClick = "green"
+        case "green":
+            greenLight.layer.opacity = 1
+            yellowLight.layer.opacity = 0.3
+            redLight.layer.opacity = 0.3
+            currentClick = "red"
+        default:
+            return
+        }
+        
     }
-
-
+    
+    override func viewDidLoad() {
+        currentClick = "red"
+        let lightBorderRadius = redLight.bounds.width / 2
+        redLight.layer.opacity = 0.3
+        yellowLight.layer.opacity = 0.3
+        greenLight.layer.opacity = 0.3
+        
+        redLight.layer.cornerRadius = lightBorderRadius
+        yellowLight.layer.cornerRadius = lightBorderRadius
+        greenLight.layer.cornerRadius = lightBorderRadius
+    }
+    
+    
 }
 
